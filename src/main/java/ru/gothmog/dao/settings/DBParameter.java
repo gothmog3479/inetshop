dd787d3526ba886b6790816c8a039fb9984836a9
gothmog3479 <gothmog79@mail.ru>
2016-12-12 11:17:26 +0300

    add db.properties

---
+++ src/main/java/ru/gothmog/dao/settings/DBParameter.java
@@ -0,0 +1,14 @@
+package ru.gothmog.dao.settings;
+
+public final class DBParameter {
+
+    public static final String DB_DRIVER = "jdbc.postgresql.driverClass";
+    public static final String DB_URL = "jdbc.postgresql.url";
+    public static final String DB_USER = "jdbc.postgresql.username";
+    public static final String DB_PASSWORD = "jdbc.postgresql.password";
+
+    public DBParameter() {
+    }
+
+
+}
