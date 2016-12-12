cdf516a74195539e6f1f553b59a1f5db5f7f50eb
gothmog3479 <gothmog79@mail.ru>
2016-12-12 12:22:49 +0300

    update db.properties update DBParameter update ConnectionPool

--- src/main/java/ru/gothmog/dao/settings/DBParameter.java
+++ src/main/java/ru/gothmog/dao/settings/DBParameter.java
@@ -6,6 +6,7 @@ public final class DBParameter {
     public static final String DB_URL = "jdbc.postgresql.url";
     public static final String DB_USER = "jdbc.postgresql.username";
     public static final String DB_PASSWORD = "jdbc.postgresql.password";
+    public static final String DB_POOLSIZE = "jdbc.postgresql.poolSize";
 
     public DBParameter() {
     }
